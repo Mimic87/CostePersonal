b433ac14d908a938bf088e4245159a0214874962
JaimeHG <jaime.h.g@hotmail.com>
2025-02-28 01:11:34 +0100

    Clase CostePersonal.java refactorizada

--- ClasePersonal.java
+++ ClasePersonal.java
@@ -1,18 +1,22 @@
 public class CostePersonal {
- 
+
         public static float costeDelPersonal(Trabajador[] trabajadores) {
                 float costeFinal = 0;
                 Trabajador trabajador;
                 final float AUMENTO_POR_EXTRA = 20;
                 for (int i = 0; i < trabajadores.length; i++) {
                         trabajador = trabajadores[i];
-	                costeFinal += trabajador.getNomina();
+                        costeFinal += trabajador.getNomina();
 
-                        if (trabajador.getTipoTrabajador() != Trabajador.DIRECTOR && trabajador.getTipoTrabajador() != Trabajador.SUBDIRECTOR) {
+                        public boolean esJefazo(Trabajador trabajador) {
+                                return trabajador.getTipoTrabajador() == Trabajador.DIRECTOR ||
+                                        trabajador.getTipoTrabajador() == Trabajador.SUBDIRECTOR;
+                        }
+
+                        if (!esJefazo(trabajador)){
                                 costeFinal += (trabajador.getHorasExtras() * AUMENTO_POR_EXTRA);
                         }
-                       
+
+                        return costeFinal;
                 }
-                return costeFinal;
         }
-}
